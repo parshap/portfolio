49af4ac2b5bcb61c7f5527fdca730ff0c6fdcc86
Parsha Pourkhomami <parshap+git@gmail.com>
2013-10-15 04:30:23 -0700

    Upload to s3 with correct headers

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,32 @@
-aws s3 cp ./dist s3://www.parshap.com/me \
-	--recursive \
+#!/usr/bin/env bash
+
+day=$(expr 60 \* 60 \* 24)
+week=$(expr $day \* 7)
+images=$(cd dist && find images -type f)
+
+# Gzip index.html
+gzip -c dist/index.html > dist/index.html.gz
+
+# Copy index.html
+aws s3 cp dist/index.html.gz \
+	s3://www.parshap.com/me/index.html \
+	--region us-east-1 \
 	--acl public-read \
-	--region us-east-1
+	--content-type "text/html; charset=utf-8" \
+	--content-encoding gzip \
+	--cache-control "max-age=$day, public" \
+	--content-language en
+
+# Remove gzipped index.html
+rm dist/index.html.gz
+
+# Copy images
+for file in $(cd dist && find images -type f)
+do
+	aws s3 cp dist/$file \
+		s3://www.parshap.com/me/$file \
+		--region us-east-1 \
+		--acl public-read \
+		--cache-control "max-age=$week, public" \
+		--content-language en
+done
