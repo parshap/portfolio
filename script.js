de4010716b016b3542b8883d1aa223d3cf6205f4
Parsha Pourkhomami <parshap+git@gmail.com>
2013-10-16 02:22:25 -0700

    Move fittext to own file

--- script.js
+++ script.js
@@ -1,28 +1,8 @@
-// jshint browser:true, globalstrict:true
+// jshint browser:true, node:true
 "use strict";
 
-// Non-jQuery port of https://github.com/davatron5000/FitText.js
-function fitText(el, options) {
-	options = options || {};
-	var scale = options.scale || 1,
-		min = parseFloat(options.minFontSize) || Number.NEGATIVE_INFINITY,
-		max = parseFloat(options.maxFontSize) || Number.POSITIVE_INFINITY,
-		k = scale / 10;
-
-	function resize() {
-		var size = el.clientWidth * k;
-		size = Math.min(size, max);
-		size = Math.max(size, min);
-		size = Math.round(size);
-		el.style.fontSize = size + "px";
-	}
-
-	resize();
-	["orientationchange", "resize"].forEach(function(event) {
-		window.addEventListener(event, resize);
-	});
-}
+var fitText = require("./lib/fittext");
 
 var q = document.querySelector.bind(document);
 
-fitText(q("#intro .heading"), { scale: 1.35 });
+fitText(q("#intro .heading"), 1.4);
