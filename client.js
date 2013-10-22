dc4ef431ca4c3dc48998a9b83b40986304aab087
Parsha Pourkhomami <parshap+git@gmail.com>
2013-10-21 20:09:06 -0700

    Do not show canvas unless supported

--- client.js
+++ client.js
@@ -4,19 +4,29 @@
 var HAS_QUERYSELECTOR = !! document.querySelector,
 	HAS_BIND = !! Function.prototype.bind;
 
-if (HAS_QUERYSELECTOR && HAS_BIND) {
-	var fitText = require("./lib/fittext"),
-		effects = require("./lib/effects");
-
-	var q = document.querySelector.bind(document),
-		id = document.getElementById.bind(document);
-
-	fitText(q("#intro .heading"), 1.4);
-
-	effects({
-		header: id("intro"),
-		projects: id("projects"),
-		bodybg: id("bg"),
-		photo: id("photo"),
-	});
+// Minimum requirements
+if ( ! HAS_QUERYSELECTOR || ! HAS_BIND) {
+	return;
 }
+
+var fitText = require("./lib/fittext"),
+	effects = require("./lib/effects");
+
+var q = document.querySelector.bind(document),
+	id = document.getElementById.bind(document);
+
+var header = id("intro"),
+	headerContainer = header.querySelector(".container"),
+	photo = id("photo");
+
+var HAS_CANVAS = photo.getContext;
+
+fitText(q("#intro .heading"), 1.4);
+
+effects({
+	header: id("intro"),
+	headerContainer: headerContainer,
+	photo: HAS_CANVAS ? photo : null,
+	projects: id("projects"),
+	bodybg: id("bg"),
+});
