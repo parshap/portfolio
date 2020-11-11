a26eca5eec183a16891a62a15eb16d307444dace
Parsha Pourkhomami <parshap+git@gmail.com>
2020-11-11 08:18:55 +0000

    Refactor deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,51 +6,64 @@ day=$(expr $hour \* 24)
 week=$(expr $day \* 7)
 week=$(expr $day \* 365)
 
-docs=("index.html" "me/index.html" "error.html" "404.html")
-images=$(cd dist && find me/images -type f)
-
-# Copy robots.txt
-aws s3 cp dist/robots.txt \
-	s3://www.parshap.com/robots.txt \
-	--region us-east-1 \
-	--acl public-read \
-	--cache-control "max-age=$year, public"
-
-# Copy html documents
-for file in ${docs[*]}
-do
-	# Copy file to S3
-	aws s3 cp dist/$file \
-		s3://www.parshap.com/$file \
+# Upload robots.txt
+upload_robots() {
+	aws s3 cp dist/robots.txt \
+		s3://www.parshap.com/robots.txt \
 		--region us-east-1 \
 		--acl public-read \
-		--content-type "text/html; charset=utf-8" \
-		--cache-control "max-age=$day, public" \
+		--cache-control "max-age=$year, public"
+}
+
+# Upload html documents
+upload_documents() {
+	local docs=("index.html" "me/index.html" "error.html" "404.html")
+	for file in ${docs[*]}
+	do
+		# Copy file to S3
+		aws s3 cp dist/$file \
+			s3://www.parshap.com/$file \
+			--region us-east-1 \
+			--acl public-read \
+			--content-type "text/html; charset=utf-8" \
+			--cache-control "max-age=$day, public" \
+			--content-language "en-us"
+	done
+}
+
+# Upload resume to S3
+upload_resume() {
+	aws s3 cp dist/parshap-resume.pdf \
+		s3://www.parshap.com/parshap-resume.pdf \
+		--region us-east-1 \
+		--acl public-read \
+		--cache-control "max-age=$hour, public" \
 		--content-language "en-us"
-done
 
-# Copy resume to S3
-aws s3 cp dist/parshap-resume.pdf \
-	s3://www.parshap.com/parshap-resume.pdf \
-	--region us-east-1 \
-	--acl public-read \
-	--cache-control "max-age=$hour, public" \
-	--content-language "en-us"
-
-# Short cache resume
-aws s3 cp dist/parshap-resume.pdf \
-	s3://www.parshap.com/parshap-resume-04a4.pdf \
-	--region us-east-1 \
-	--acl public-read \
-	--cache-control "max-age=$minute, public" \
-	--content-language "en-us"
-
-# Copy images
-for file in $images
-do
-	aws s3 cp dist/$file \
-		s3://www.parshap.com/$file \
+	# Short cache resume
+	aws s3 cp dist/parshap-resume.pdf \
+		s3://www.parshap.com/parshap-resume-04a4.pdf \
 		--region us-east-1 \
 		--acl public-read \
-		--cache-control "max-age=$week, public"
-done
+		--cache-control "max-age=$minute, public" \
+		--content-language "en-us"
+}
+
+# Upload images
+upload_images() {
+	local images=$(cd dist && find me/images -type f)
+
+	for file in $images
+	do
+		aws s3 cp dist/$file \
+			s3://www.parshap.com/$file \
+			--region us-east-1 \
+			--acl public-read \
+			--cache-control "max-age=$week, public"
+	done
+}
+
+upload_robots
+upload_documents
+upload_resume
+upload_images
