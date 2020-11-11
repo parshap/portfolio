948bb3dbaba3e0b989cb66f7cc475a770443c2e4
Parsha Pourkhomami <parshap+git@gmail.com>
2020-11-11 07:33:34 +0000

    Remove manual gzipping

--- deploy.sh
+++ deploy.sh
@@ -17,39 +17,23 @@ aws s3 cp dist/robots.txt \
 # Copy html documents
 for file in ${docs[*]}
 do
-	# Gzip file and save to a temp file
-	echo "Compressing $file (gzip)"
-	gzip -c dist/$file > dist/$file.gz
-
 	# Copy file to S3
-	aws s3 cp dist/$file.gz \
+	aws s3 cp dist/$file \
 		s3://www.parshap.com/$file \
 		--region us-east-1 \
 		--acl public-read \
 		--content-type "text/html; charset=utf-8" \
-		--content-encoding gzip \
 		--cache-control "max-age=$day, public" \
 		--content-language en
-
-	# Remove gzip temp file
-	rm dist/$file.gz
 done
 
-# Compress resume
-echo "Compressing parshap-resume.pdf (gzip)"
-gzip -c dist/parshap-resume.pdf > dist/parshap-resume.pdf.gz
-
 # Copy resume to S3
-aws s3 cp dist/parshap-resume.pdf.gz \
+aws s3 cp dist/parshap-resume.pdf \
 	s3://www.parshap.com/parshap-resume.pdf \
 	--region us-east-1 \
 	--acl public-read \
-	--content-encoding gzip \
 	--cache-control "max-age=$day, public" \
 	--content-language en
-
-# Remove resume gzip temp file
-rm dist/parshap-resume.pdf.gz
 
 # Copy images
 for file in $images
@@ -58,5 +42,5 @@ do
 		s3://www.parshap.com/$file \
 		--region us-east-1 \
 		--acl public-read \
-		--cache-control "max-age=$week, public" 
+		--cache-control "max-age=$week, public"
 done
